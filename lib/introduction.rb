535f84d7e8ba56d6dcaccee1938788a2c2e210b2
Keith Isham <kisham8899@gmail.com>
2019-04-16 19:02:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,6 @@ def introduction_with_language(name, language)
   name = "dan"
   language = "Ember.js"
 
-  puts "Hi, my name is #{name} and I am learning "
+  puts "Hi, my name is #{name} and I am learning #{language}"
 
 end
